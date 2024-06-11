94655fd57f9a5eb2649283ac0da549eb3dd8e0ad
Vinicius040496 <vinicius.almeida2011@gmail.com>
2024-06-11 18:32:08 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp139/Calculadora.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp139
+{
+    internal class Calculadora   
+    {
+            public static int Soma(params int[] numbers)
+            {
+                int sum = 0;
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    sum += numbers[i];
+                }
+                return sum;
+            }
+        }
+    }
---
+++ ConsoleApp139/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp139
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int V1 = Calculadora.Soma(new int[] { 30, 50 });
+
+            Console.WriteLine(V1);
+            Console.ReadLine();
+        }
+    }
+}
